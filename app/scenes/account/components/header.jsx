80de2cca07375921530924d0f0ea27dae332d9d1
Gabriel Furlan <gabrielfurlan05@gmail.com>
2017-03-03 18:58:35 -0300

    Adicionando Nav na sessão de Account

--- app/scenes/account/components/header.jsx
+++ app/scenes/account/components/header.jsx
@@ -4,9 +4,14 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header className="profile">
+				<nav className="nav">
+					<a href="#" className="icon --nav" />
+					<a href="#" className="brand" />
+					<a href="#" className="icon --globe"/>
+				</nav>
 				<img className="image" src="http://lorempixel.com/200/200/people/" />
-				<h1 className="name">Gabriel Furlan</h1>
-				<h2 className="email">gabrielfurlan05@gmail.com</h2>
+				<h1 className="name">Cacilds Vidis</h1>
+				<h2 className="email">cacildsvidis@gmail.com</h2>
 			</header>
 		);
 	}
